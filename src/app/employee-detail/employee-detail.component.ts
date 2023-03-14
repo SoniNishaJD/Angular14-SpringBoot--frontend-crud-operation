02f6328d69753d99910914fbd1a9240c47f660f9
Soni Nisha <soni.nishaa24@gmail.com>
2023-03-14 13:16:26 +0530

    first commit

---
+++ src/app/employee-detail/employee-detail.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-employee-detail',
+  templateUrl: './employee-detail.component.html',
+  styleUrls: ['./employee-detail.component.css']
+})
+export class EmployeeDetailComponent {
+
+  id!: number
+  employee!: Employee
+  constructor(private route: ActivatedRoute, private employeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.employee = new Employee();
+    this.employeService.getEmployeeById(this.id).subscribe( data => {
+      this.employee = data;
+    });
+  }
+
+}
